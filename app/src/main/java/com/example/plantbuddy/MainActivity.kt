de6524cc6f68d4170c3ddfd69eeaf0470e1b7bcc
Matic Lukežič <maticlukezic007@gmail.com>
2023-12-04 12:33:57 +0100

    Small UI change

--- app/src/main/java/com/example/plantbuddy/MainActivity.kt
+++ app/src/main/java/com/example/plantbuddy/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.plantbuddy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.example.lib.Plant
 import com.example.plantbuddy.databinding.ActivityMainBinding
@@ -24,14 +25,26 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.fragment, PlantFragment())
             .commit()
 
-        binding.fabAdd.setOnClickListener {
-           supportFragmentManager.beginTransaction()
-               .replace(R.id.fragment, InputFragment())
-               .addToBackStack(null)
-               .commit()
+        binding.buttonAdd.setOnClickListener {
+            hideFirstPlantCardView()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment, InputFragment())
+                .addToBackStack(null)
+                .commit()
         }
 
-        supportFragmentManager.setFragmentResultListener("editRequestKey", this) { requestKey, bundle ->
+        binding.fabAdd.setOnClickListener {
+            hideFirstPlantCardView()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment, InputFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        supportFragmentManager.setFragmentResultListener(
+            "editRequestKey",
+            this
+        ) { requestKey, bundle ->
             val id = bundle.getString("plantID")
             val name = bundle.getString("name")
             val plant = Plant(name!!, id!!)
@@ -39,20 +52,36 @@ class MainActivity : AppCompatActivity() {
             plantFragment?.updatePlants(app.getPlantPosition(plant))
         }
 
-        supportFragmentManager.setFragmentResultListener("addRequestKey", this) { requestKey, bundle ->
+        supportFragmentManager.setFragmentResultListener(
+            "addRequestKey",
+            this
+        ) { requestKey, bundle ->
             val name = bundle.getString("name")
             val plant = Plant(name!!)
             app.addPlant(plant)
+            hideFirstPlantCardView()
         }
 
         binding.fabHome.setOnClickListener {
-            if(plantFragment == null){
+            if (plantFragment == null) {
                 plantFragment = PlantFragment()
             }
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment, plantFragment!!)
                 .addToBackStack(null)
                 .commit()
+            if(app.getAllPlants().isEmpty()) {
+                binding.cardViewAddFirstPlant.visibility = View.VISIBLE
+                binding.buttonAdd.visibility = View.VISIBLE
+            }
         }
+    }
+
+    private fun hideFirstPlantCardView() {
+        if(app.getAllPlants().isNotEmpty()) {
+            return
+        }
+        binding.cardViewAddFirstPlant.visibility = View.GONE
+        binding.buttonAdd.visibility = View.GONE
     }
 }
